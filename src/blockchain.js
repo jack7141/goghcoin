623355262e070bdbdac3588c4e6890f05d88a939
Gogh Hwang <ghl92479@gmail.com>
2018-04-26 16:58:09 +0900

    final

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,135 +1,316 @@
-const CryptoJS = require("crypto-js");//암호악에 쓰이느는 것 yarn add cropyo js
+const CryptoJS = require("crypto-js"),
+  _ = require("lodash"),
+  Wallet = require("./wallet"),
+  Mempool = require("./memPool"),
+  Transactions = require("./transactions"),
+  hexToBinary = require("hex-to-binary");
 
-class Block{
-    constructor(index, hash, previousHash, timestamp, data){//블록구조를 만들고 내부에 해쉬를 만드는 과정이였다. 해쉬는 수학함수로서 입력값을 넣으면 아웃풋이 생김
-        this.index = index;
-        this.hash =  hash;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.data = data;
-    }
+const {
+  getBalance,
+  getPublicFromWallet,
+  createTx,
+  getPrivateFromWallet
+} = Wallet;
+
+const { createCoinbaseTx, processTxs } = Transactions;
+
+const { addToMempool, getMempool, updateMempool } = Mempool;
+
+const BLOCK_GENERATION_INTERVAL = 10;
+const DIFFICULTY_ADJUSMENT_INTERVAL = 10;
+
+class Block {
+  constructor(index, hash, previousHash, timestamp, data, difficulty, nonce) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
+  }
 }
 
+const genesisTx = {
+  txIns: [{ signature: "", txOutId: "", txOutIndex: 0 }],
+  txOuts: [
+    {
+      address:
+        "04f20aec39b4c5f79355c053fdaf30410820400bb83ad93dd8ff16834b555e0f6262efba6ea94a87d3c267b5e6aca433ca89b342ac95c40230349ea4bf9caff1ed",
+      amount: 50
+    }
+  ],
+  id: "ad67c73cd8e98af6db4ac14cc790664a890286d4b06c6da7ef223aef8c281e76"
+};
+
 const genesisBlock = new Block(
-    0,//인덱스
-    "d7d0b47b27a2d9061b55356f5ace714fc121a9e7fa0aa4e6e3972bd408b1a332",//해쉬
-    null,//프리비우스 해쉬
-    1520962144289,//new date().getTime()  타임스탬프
-    "This is the genesis!!"//데이터
+  0,
+  "82a3ecd4e76576fccce9999d560a31c7ad1faff4a3f4c6e7507a227781a8537f",
+  "",
+  1518512316,
+  [genesisTx],
+  0,
+  0
 );
 
 let blockchain = [genesisBlock];
 
-const getLastBlock = () => blockchain[blockchain.length -1];
+let uTxOuts = processTxs(blockchain[0].data, [], 0);
 
-const getTimestamp =() => new Date().getTime()/1000;
+const getNewestBlock = () => blockchain[blockchain.length - 1];
+
+const getTimestamp = () => Math.round(new Date().getTime() / 1000);
 
 const getBlockchain = () => blockchain;
 
+const createHash = (index, previousHash, timestamp, data, difficulty, nonce) =>
+  CryptoJS.SHA256(
+    index + previousHash + timestamp + JSON.stringify(data) + difficulty + nonce
+  ).toString();
 
-//암호학 크리토js를 불러오면 인풋을 해줘야한다. 그래서 인덱스 프리브우스 해쉬 등을 넣어주고 바로 리턴 =>해서 받아준다.
-const createHash = (index , previousHash, timestamp, data) =>
-    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();//data를 스트링으로 변환시켜주는건 숫자로도 데이터를 넣을수 있게함으로!, 그러나 투스트링으로 변환시켜준다고 생각할수 있느데 insNewStructureValid함수에서 각각 검사를 하므로 스트링으로 변환시켜주는게 안정적이다.
+const createNewBlock = () => {
+  const coinbaseTx = createCoinbaseTx(
+    getPublicFromWallet(),
+    getNewestBlock().index + 1
+  );
+  const blockData = [coinbaseTx].concat(getMempool());
+  return createNewRawBlock(blockData);
+};
 
-//새로운 블록을 생성하는 함수를 만들어준다~    
-const createNewBlock = data => {
-    const previousBlock  = getLastBlock();
-    const newBlockIndex  = previousBlock.index + 1;//기존의 블록말고 그 다음 블록에 데이터를 넣어야 하므로 +1을 해준다.
-    const newTimestamp  = getTimestamp();
-    const newHash  = createHash(
-        newBlockIndex,
-        previousBlock.hash,
-        newTimestamp,
-        data
+const createNewRawBlock = data => {
+  const previousBlock = getNewestBlock();
+  const newBlockIndex = previousBlock.index + 1;
+  const newTimestamp = getTimestamp();
+  const difficulty = findDifficulty();
+  const newBlock = findBlock(
+    newBlockIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data,
+    difficulty
+  );
+  addBlockToChain(newBlock);
+  require("./p2p").broadcastNewBlock();
+  return newBlock;
+};
+
+const findDifficulty = () => {
+  const newestBlock = getNewestBlock();
+  if (
+    newestBlock.index % DIFFICULTY_ADJUSMENT_INTERVAL === 0 &&
+    newestBlock.index !== 0
+  ) {
+    return calculateNewDifficulty(newestBlock, getBlockchain());
+  } else {
+    return newestBlock.difficulty;
+  }
+};
+
+const calculateNewDifficulty = (newestBlock, blockchain) => {
+  const lastCalculatedBlock =
+    blockchain[blockchain.length - DIFFICULTY_ADJUSMENT_INTERVAL];
+  const timeExpected =
+    BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSMENT_INTERVAL;
+  const timeTaken = newestBlock.timestamp - lastCalculatedBlock.timestamp;
+  if (timeTaken < timeExpected / 2) {
+    return lastCalculatedBlock.difficulty + 1;
+  } else if (timeTaken > timeExpected * 2) {
+    return lastCalculatedBlock.difficulty - 1;
+  } else {
+    return lastCalculatedBlock.difficulty;
+  }
+};
+
+const findBlock = (index, previousHash, timestamp, data, difficulty) => {
+  let nonce = 0;
+  while (true) {
+    console.log("Current nonce", nonce);
+    const hash = createHash(
+      index,
+      previousHash,
+      timestamp,
+      data,
+      difficulty,
+      nonce
     );
-    const newBlock = new Block(
-        newBlockIndex,
-        newHash,
-        previousBlock.hash,
-        newTimestamp,
-        data
-    );
-    addBlockToChain(newBlock);
-    return newBlock;
+    if (hashMatchesDifficulty(hash, difficulty)) {
+      return new Block(
+        index,
+        hash,
+        previousHash,
+        timestamp,
+        data,
+        difficulty,
+        nonce
+      );
+    }
+    nonce++;
+  }
+};
+
+const hashMatchesDifficulty = (hash, difficulty) => {
+  const hashInBinary = hexToBinary(hash);
+  const requiredZeros = "0".repeat(difficulty);
+  console.log("Trying difficulty:", difficulty, "with hash", hashInBinary);
+  return hashInBinary.startsWith(requiredZeros);
 };
 
 const getBlocksHash = block =>
-   createHash(block.index, block.previousHash, block.timestamp, block.data);
- 
- const isNewBlockValid = (candidateBlock, latestBlock) => {
-   if (!isNewStructureValid(candidateBlock)) {
-     console.log("The candidate block structure is not valid");
-     return false;
-   } else if (latestBlock.index + 1 !== candidateBlock.index) {
-     console.log("The candidate block doesnt have a valid index");
-     return false;
-   } else if (latestBlock.hash !== candidateBlock.previousHash) {
-     console.log(
-       "The previousHash of the candidate block is not the hash of the latest block"
-     );
-     return false;
-   } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
-     console.log("The hash of this block is invalid");
-     return false;
-   }
-   return true;
- };
- //블록의 구조를 검증하는 펑션을 하나 만든다.
- const isNewStructureValid = block => {
-   return (
-     typeof block.index === "number" &&
-     typeof block.hash === "string" &&
-     typeof block.previousHash === "string" &&
-     typeof block.timestamp === "number" &&
-     typeof block.data === "string"
-   );
- };
- 
-//체인을 검증하기 위함, 타인에게 릴리즈했을경우 교체?하기 위함??......
-//두개의 블록체인은 같은 하나의 제네시스 출신이어야 한다..!!매우중요!!
+  createHash(
+    block.index,
+    block.previousHash,
+    block.timestamp,
+    block.data,
+    block.difficulty,
+    block.nonce
+  );
 
- const isChainValid = candidateChain => {
-   const isGenesisValid = block => {
-     return JSON.stringify(block) === JSON.stringify(genesisBlock);
-   };
-   if (!isGenesisValid(candidateChain[0])) {
-     console.log(
-       "The candidateChains's genesisBlock is not the same as our genesisBlock"
-     );
-     return false;
-   }
-   //블록은 첫번째는 체크하지 않는다 첫번째 블록은 제네시스 블록 즉 최초의 블록이므로 건들지 않고 두번쨰 블록부터 검사한다.
-   for (let i = 1; i < candidateChain.length; i++) {
-     if (!isNewBlockValid(candidateChain[i], candidateChain[i - 1])) {
-       return false;
-     }
-   }
-   return true;
- };
- //블록체인이 유효하다면 교환을 해주는 함수를 만들어 줘야함
- const replaceChain = candidateChain => {
-   if (
-     isChainValid(candidateChain) &&
-     candidateChain.length > getBlockchain().length
-   ) {
-     blockchain = candidateChain;
-     return true;
-   } else {
-     return false;
-   }
- };
- 
- const addBlockToChain = candidateBlock => {
-   if (isNewBlockValid(candidateBlock, getLastBlock())) {
-     blockchain.push(candidateBlock);
-     return true;
-   } else {
-     return false;
-   }
- };
-//서버를 생성하기전에 export를 해줘야한다.
+const isTimeStampValid = (newBlock, oldBlock) => {
+  return (
+    oldBlock.timestamp - 60 < newBlock.timestamp &&
+    newBlock.timestamp - 60 < getTimestamp()
+  );
+};
+
+const isBlockValid = (candidateBlock, latestBlock) => {
+  if (!isBlockStructureValid(candidateBlock)) {
+    console.log("The candidate block structure is not valid");
+    return false;
+  } else if (latestBlock.index + 1 !== candidateBlock.index) {
+    console.log("The candidate block doesnt have a valid index");
+    return false;
+  } else if (latestBlock.hash !== candidateBlock.previousHash) {
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
+    return false;
+  } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
+    console.log("The hash of this block is invalid");
+    return false;
+  } else if (!isTimeStampValid(candidateBlock, latestBlock)) {
+    console.log("The timestamp of this block is dodgy");
+    return false;
+  }
+  return true;
+};
+
+const isBlockStructureValid = block => {
+  return (
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timestamp === "number" &&
+    typeof block.data === "object"
+  );
+};
+
+const isChainValid = candidateChain => {
+  const isGenesisValid = block => {
+    return JSON.stringify(block) === JSON.stringify(genesisBlock);
+  };
+  if (!isGenesisValid(candidateChain[0])) {
+    console.log(
+      "The candidateChains's genesisBlock is not the same as our genesisBlock"
+    );
+    return null;
+  }
+
+  let foreignUTxOuts = [];
+
+  for (let i = 0; i < candidateChain.length; i++) {
+    const currentBlock = candidateChain[i];
+    if (i !== 0 && !isBlockValid(currentBlock, candidateChain[i - 1])) {
+      return null;
+    }
+
+    foreignUTxOuts = processTxs(
+      currentBlock.data,
+      foreignUTxOuts,
+      currentBlock.index
+    );
+
+    if (foreignUTxOuts === null) {
+      return null;
+    }
+  }
+  return foreignUTxOuts;
+};
+
+const sumDifficulty = anyBlockchain =>
+  anyBlockchain
+    .map(block => block.difficulty)
+    .map(difficulty => Math.pow(2, difficulty))
+    .reduce((a, b) => a + b);
+
+const replaceChain = candidateChain => {
+  const foreignUTxOuts = isChainValid(candidateChain);
+  const validChain = foreignUTxOuts !== null;
+  if (
+    validChain &&
+    sumDifficulty(candidateChain) > sumDifficulty(getBlockchain())
+  ) {
+    blockchain = candidateChain;
+    uTxOuts = foreignUTxOuts;
+    updateMempool(uTxOuts);
+    require("./p2p").broadcastNewBlock();
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const addBlockToChain = candidateBlock => {
+  if (isBlockValid(candidateBlock, getNewestBlock())) {
+    const processedTxs = processTxs(
+      candidateBlock.data,
+      uTxOuts,
+      candidateBlock.index
+    );
+    if (processedTxs === null) {
+      console.log("Couldnt process txs");
+      return false;
+    } else {
+      blockchain.push(candidateBlock);
+      uTxOuts = processedTxs;
+      updateMempool(uTxOuts);
+      return true;
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getUTxOutList = () => _.cloneDeep(uTxOuts);
+
+const getAccountBalance = () => getBalance(getPublicFromWallet(), uTxOuts);
+
+const sendTx = (address, amount) => {
+  const tx = createTx(
+    address,
+    amount,
+    getPrivateFromWallet(),
+    getUTxOutList(),
+    getMempool()
+  );
+  addToMempool(tx, getUTxOutList());
+  require("./p2p").broadcastMempool(); // <--- new line
+  return tx;
+};
+
+const handleIncomingTx = tx => {
+  addToMempool(tx, getUTxOutList()); // <-- new line
+};
+
 module.exports = {
+  getNewestBlock,
   getBlockchain,
   createNewBlock,
-  getLastBlock
+  isBlockStructureValid,
+  addBlockToChain,
+  replaceChain,
+  getAccountBalance,
+  sendTx,
+  handleIncomingTx,
+  getUTxOutList
 };
--- src/server.js
+++ src/server.js
@@ -1,35 +1,90 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const Blockchain = require("./blockchain");
-const P2P = require("./p2p");
+const express = require("express"),
+  _ = require("lodash"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  Blockchain = require("./blockchain"),
+  P2P = require("./p2p"),
+  Mempool = require("./memPool"),
+  Wallet = require("./wallet");
 
-const { getBlockchain, createNewBlock } = Blockchain;
+const { getBlockchain, createNewBlock, getAccountBalance, sendTx, getUTxOutList } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
+const { initWallet, getPublicFromWallet, getBalance } = Wallet;
+const { getMempool } = Mempool;
 
+// Psssst. Don't forget about typing 'export HTTP_PORT=4000' in your console
 const PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
+app.use(cors());
 
-app.get("/blocks", (req, res) => {
+app
+  .route("/blocks")
+  .get((req, res) => {
     res.send(getBlockchain());
-});
-app.post("/blocks", (req, res) => {
-    const { body: {data} } = req;
-    const newBlock = createNewBlock(data);
+  })
+  .post((req, res) => {
+    const newBlock = createNewBlock();
     res.send(newBlock);
-});
+  });
 
 app.post("/peers", (req, res) => {
-    const { body : { peer }  } = req;
-    connectToPeers(peer);
-    res.send();
+  const { body: { peer } } = req;
+  connectToPeers(peer);
+  res.send();
 });
 
-const server = app.listen(PORT, () => 
-    console.log(`GoghCoin Server running on port ${PORT} `)
+app.get("/me/balance", (req, res) => {
+  const balance = getAccountBalance();
+  res.send({ balance });
+});
+
+app.get("/me/address", (req, res) => {
+  res.send(getPublicFromWallet());
+});
+
+app.get("/blocks/:hash", (req, res) => {
+  const { params : { hash } } = req;
+  const block = _.find(getBlockchain(), { hash });
+  if( block === undefined){
+    res.status(400).send("Block not found");
+  } else {
+    res.send(block);
+  }
+});
+
+app
+  .route("/transactions")
+  .get((req, res) => {
+    res.send(getMempool());
+  })
+  .post((req, res) => {
+    try {
+      const { body: { address, amount } } = req;
+      if (address === undefined || amount === undefined) {
+        throw Error("Please specify and address and an amount");
+      } else {
+        const resPonse = sendTx(address, amount);
+        res.send(resPonse);
+      }
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+app.get("/address/:address", (req, res) => {
+  const { params:  {address } } =req;
+  const balance = getBalance(address, getUTxOutList());
+  res.send({ balance });
+});
+
+
+const server = app.listen(PORT, () =>
+  console.log(`Nomadcoin HTTP Server running on port ${PORT} ✅`)
 );
 
+initWallet();
 startP2PServer(server);
